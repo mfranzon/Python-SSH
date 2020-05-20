561a87162fa1c606b75bc9372f6944db52a32dcd
marco <mfranzon@sissa.it>
2020-05-20 16:22:47 +0200

    interesting functions

---
+++ config.py
@@ -0,0 +1,9 @@
+"""Configuration module."""
+
+host = ""
+user = ""
+ssh_key_filepath = ""
+
+local_file_directory=""
+remote_path = "" #The path to the remote directory we're looking to target for file transfers.
+local_path = '/data' #Same idea as above, but the reverse
